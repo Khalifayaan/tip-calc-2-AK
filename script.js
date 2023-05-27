263b5703be719234bf5c21e96b44cd365c3f7359
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-05-27 22:08:01 +0300

    Tip assignment 2 - Done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+
+
+function calcTip(bill) {
+    let tip;
+  
+    if (bill >= 50 && bill <= 300) {
+      tip = bill * 0.15; // 15% tip
+    } else {
+      tip = bill * 0.20; // 20% tip
+    }
+  
+    return tip;
+  }
+  
+  // Bill amount
+  let bill = 500;
+  let tipAmount = calcTip(bill);
+  console.log("The bill is " + bill + ", the tip amount is " + tipAmount);
+  
